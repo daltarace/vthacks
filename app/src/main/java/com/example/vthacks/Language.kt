e8c26382662e2a88c31e96cff84bcc51894ceb98
Ria Vadhavkar <vadhavkar.ria@gmail.com>
2022-02-26 05:18:52 -0500

    added comments

--- app/src/main/java/com/example/vthacks/Language.kt
+++ app/src/main/java/com/example/vthacks/Language.kt
@@ -2,9 +2,9 @@ package com.example.vthacks
 
 // this is the Language model class
 class Language(
-    val classname: String = "",
-    val classcode: String = "",
-    val teacher: String = "",
+    val classname: String = "", //name
+    val classcode: String = "", //subject + courseNumber
+    val teacher: String = "", //instructor
     val gpa: String = "",
     val wiki: String = "",
 
